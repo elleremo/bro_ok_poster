66766700fde96b74f78e97a1a0f5d04a48c95d1c
elleremo <elleremo@yandex.ru>
2019-03-03 11:05:21 +0300

    add class

---
+++ BroOkAutoPost.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: OK Poster
+Author: elleremo
+Version: 1.0.0
+*/
+
+class BroOkAutoPost {
+
+
+}
+
+function bro_ok_poster__init() {
+
+}
+
+add_action( 'plugins_loaded', 'bro_ok_poster__init', 50 );
