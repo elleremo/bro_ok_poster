679f58e68ace78f3d4fdec86aa95fab15595ef1d
elleremo <elleremo@yandex.ru>
2019-05-11 11:41:41 +0300

    early exit

--- BroOkAutoPost.php
+++ BroOkAutoPost.php
@@ -6,34 +6,40 @@ Author: elleremo
 Version: 1.0.0
 */
 
-class BroOkAutoPost {
+class BroOkAutoPost
+{
 
     public static $accessToken;
     public static $privateKey;  // Секретный ключ приложения
     public static $publicKey;  // Публичный ключ приложения
     public static $groupID;  // ID нашей группы
 
-    public function __construct() {
+    public function __construct()
+    {
 
-        self::$accessToken = get_option( 'ok_app_ACCESS_TOKEN', false );
-        self::$privateKey  = get_option( 'ok_app_PRIVATE_KEY', false );
-        self::$publicKey   = get_option( 'ok_app_PUBLIC_KEY', false );
-        self::$groupID     = get_option( 'ok_app_GROUP_ID', false );
+        self::$accessToken = get_option('ok_app_ACCESS_TOKEN', false);
+        self::$privateKey = get_option('ok_app_PRIVATE_KEY', false);
+        self::$publicKey = get_option('ok_app_PUBLIC_KEY', false);
+        self::$groupID = get_option('ok_app_GROUP_ID', false);
 
-        add_action( 'transition_post_status', [ __CLASS__, 'sendText' ], 10, 3 );
+        if (empty(self::$accessToken) || empty(self::$privateKey) || empty(self::$publicKey) || empty(self::$groupID)) {
+            return false;
+        }
 
+        add_action('transition_post_status', [__CLASS__, 'sendText'], 10, 3);
     }
 
-    public static function sendText($new_status, $oldStatus, $post ) {
-        if ( 'publish' === $new_status  && 'publish' !== $oldStatus ) {
-            self::push( $post );
+    public static function sendText($new_status, $oldStatus, $post)
+    {
+        if ('publish' === $new_status && 'publish' !== $oldStatus) {
+            self::push($post);
         }
     }
 
+    static function push($post)
+    {
 
-    static function push( $post ) {
-
-        $link       = get_permalink( $post );
+        $link = get_permalink($post);
         $attachment = '{
                     "media": [
                         {
@@ -53,29 +59,30 @@ class BroOkAutoPost {
 
         $params = array(
             "application_key" => self::$publicKey,
-            "method"          => "mediatopic.post",
-            "gid"             => self::$groupID,
-            "type"            => "GROUP_THEME",
-            "attachment"      => $attachment,
-            "format"          => "json",
+            "method" => "mediatopic.post",
+            "gid" => self::$groupID,
+            "type" => "GROUP_THEME",
+            "attachment" => $attachment,
+            "format" => "json",
         );
 
-        $sig = md5( self::arInStr( $params ) . md5( self::$accessToken . self::$privateKey ) );
+        $sig = md5(self::arInStr($params) . md5(self::$accessToken . self::$privateKey));
 
         $params['access_token'] = self::$accessToken;
-        $params['sig']          = $sig;
+        $params['sig'] = $sig;
 
-        return self::http( "https://api.ok.ru/fb.do", $params );
+        return self::http("https://api.ok.ru/fb.do", $params);
     }
 
-    static function arInStr( $array ) {
-        ksort( $array );
+    static function arInStr($array)
+    {
+        ksort($array);
 
         $string = "";
 
-        foreach ( $array as $key => $val ) {
-            if ( is_array( $val ) ) {
-                $string .= $key . "=" . self::arInStr( $val );
+        foreach ($array as $key => $val) {
+            if (is_array($val)) {
+                $string .= $key . "=" . self::arInStr($val);
             } else {
                 $string .= $key . "=" . $val;
             }
@@ -84,32 +91,34 @@ class BroOkAutoPost {
         return $string;
     }
 
-    public static function http( $url, $params = array() ) {
-        $response = wp_remote_post( $url, array(
-            'method'      => 'POST',
-            'timeout'     => 45,
+    public static function http($url, $params = array())
+    {
+        $response = wp_remote_post($url, array(
+            'method' => 'POST',
+            'timeout' => 45,
             'redirection' => 5,
             'httpversion' => '1.0',
-            'blocking'    => true,
-            'headers'     => array(),
-            'body'        => $params,
-            'cookies'     => array()
-        ) );
+            'blocking' => true,
+            'headers' => array(),
+            'body' => $params,
+            'cookies' => array()
+        ));
 
         return $response;
     }
 
 }
 
-function bro_ok_poster__init() {
+function bro_ok_poster__init()
+{
 
     new BroOkAutoPost();
 }
 
-add_action( 'plugins_loaded', 'bro_ok_poster__init', 20 );
+add_action('plugins_loaded', 'bro_ok_poster__init', 20);
 
 
-add_action( 'plugins_loaded', function () {
-    require_once( plugin_dir_path( __FILE__ ) . 'include/addSettingsPageOK.php' );
+add_action('plugins_loaded', function () {
+    require_once(plugin_dir_path(__FILE__) . 'include/addSettingsPageOK.php');
     new addSettingsPageOK();
-} );
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+#WordPress Ok Poster
+
+1. ACCESS_TOKEN
+2. PRIVATE_KEY
+3. PUBLIC_KEY
+
+ID группы нужно указать обязательно
